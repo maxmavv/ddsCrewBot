368fe1d1235f0fb7304a064aa77bc0f221444424
causelovem <rat.taurus@gmail.com>
2018-11-09 10:28:13 +0300

    some more fixes

--- bot.py
+++ bot.py
@@ -182,8 +182,8 @@ def sqlsql(message):
         # if sqlQuery.upper().startswith('SELECT'):
         if user == 230563389:
             res = db.sql_exec(sqlQuery, [])
-            # print(str(res))
-            resStr = ''
+            print(str(res))
+            resStr = '[]'
             if res == 'ERROR!':
                 resStr = 'Ошибка в SQL запросе!'
             else:
